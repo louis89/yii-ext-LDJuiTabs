32b58f2f5558a5a359d24316ea064f1a92041137
Louis DaPrato <l.daprato@gmail.com>
2014-05-01 21:07:46 -0500

    Updated .gitignore, added evaled tab names, updated css

--- LDJuiTabs/LDJuiTabs.php
+++ LDJuiTabs/LDJuiTabs.php
@@ -22,36 +22,100 @@ class LDJuiTabs extends CJuiTabs
 	public $vertical = false;
 	
 	/**
-	 * (non-PHPdoc)
-	 * @see CJuiTabs::run()
+	 * @var string A php expression that is evaluated for each tab header the result of which is used in place of the header template. 
+	 * 3 variables are provided in the eval'd expression $title (the title of the tab), $content (an array containing the content configuration), $template (string the header template).
+	 * If null this expression is ignored and the headerTemplate property's value is used instead.
+	 * Defaults to null.
+	 */
+	public $headerTemplateExpression;
+	
+	/**
+	 * @var string A php expression that is evaluated for each tab's content the result of which is used in place of the content template. 
+	 * 3 variables are provided in the eval'd expression $title (the title of the tab), $content (an array containing the content configuration), $template (string the content template).
+	 * If null this expression is ignored and the headerTemplate property's value is used instead.
+	 * Defaults to null.
+	 */
+	public $contentTemplateExpression;
+	
+	/**
+	 * Run this widget.
+	 * This method registers necessary javascript and renders the needed HTML code.
 	 */
 	public function run()
 	{
-		parent::run();
+		$id = $this->getId();
+		if(isset($this->htmlOptions['id']))
+		{
+			$id = $this->htmlOptions['id'];
+		}
+		else
+		{
+			$this->htmlOptions['id'] = $id;
+		}
+	
+		echo CHtml::openTag($this->tagName, $this->htmlOptions)."\n";
+		
+		$this->renderTabs($id);
+		
+		echo CHtml::closeTag($this->tagName)."\n";
+	
+		$options = CJavaScript::encode($this->options);
+		$clientScript = Yii::app()->getClientScript();
+		$clientScript->registerScript(__CLASS__.'#'.$id, "jQuery('#{$id}').tabs($options);");
+		
 		if($this->vertical)
 		{
-			$id = $this->getId();
-			if(isset($this->htmlOptions['id']))
+			$assetsDir = dirname(__FILE__).DIRECTORY_SEPARATOR.'assets';
+			if(is_dir($assetsDir))
 			{
-				$id = $this->htmlOptions['id'];
+				$assetsUrl = Yii::app()->getAssetManager()->publish($assetsDir, false, 1, defined('YII_DEBUG') && YII_DEBUG);
+				$clientScript->registerCssFile($assetsUrl.'/jquery.ui.tabs.vertical.css');
+				$clientScript->registerScript(
+						__CLASS__.'#'.$id.'-vertical',
+						'jQuery("#'.$id.'").addClass("ui-tabs-vertical ui-helper-clearfix");'.
+						'jQuery("#'.$id.' li").removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );'
+				);
+			}
+		}
+	}
+	
+	public function renderTabs($id)
+	{	
+		$tabsOut = "";
+		$contentOut = "";
+		$tabCount = 0;
+		
+		foreach($this->tabs as $title => $content)
+		{
+			if(!is_array($content))
+			{
+				$content = array('content' => $content);
+			}
+			
+			if(!isset($content['id']))
+			{
+				$content['id'] = $id.'_tab_'.$tabCount++;
+			}
+
+			$headerTemplate = $this->headerTemplateExpression !== null ? $this->evaluateExpression($this->headerTemplateExpression, array('template' => $this->headerTemplate, 'title' => $title, 'content' => $content)) : $this->headerTemplate;
+		
+			if(isset($content['ajax']))
+			{
+				$tabsOut .= strtr($headerTemplate, array('{title}' => $title, '{url}' => CHtml::normalizeUrl($content['ajax']), '{id}' => $content['id']))."\n";
 			}
 			else
 			{
-				$this->htmlOptions['id'] = $id;
+				$tabsOut .= strtr($headerTemplate, array('{title}' => $title, '{url}' => '#'.$content['id'], '{id}' => $content['id']))."\n";
+				if(isset($content['content']))
+				{
+					$contentTemplate = $this->contentTemplateExpression !== null ? $this->evaluateExpression($this->contentTemplateExpression, array('template' => $this->contentTemplate, 'title' => $title, 'content' => $content)) : $this->contentTemplate;
+					$contentOut .= strtr($contentTemplate, array('{content}' => $content['content'], '{id}' => $content['id']))."\n";
+				}
 			}
-			$assetsDir = dirname(__FILE__).DIRECTORY_SEPARATOR.'assets';
-			if(is_dir($assetsDir)) 
-			{
-				$assetsUrl = Yii::app()->getAssetManager()->publish($assetsDir, false, 1, YII_DEBUG);
-			}
-			$clientScript = Yii::app()->getClientScript();
-			$clientScript->registerCssFile($assetsUrl.'/jquery.ui.tabs.vertical.css');
-			$clientScript->registerScript(
-					__CLASS__.'#'.$id.'-vertical', 
-					'jQuery("#'.$id.'").addClass("ui-tabs-vertical ui-helper-clearfix");'.
-					'jQuery("#'.$id.' li").removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );'
-			);
 		}
+		
+		echo "<ul>\n".$tabsOut."</ul>\n";
+		echo $contentOut;
 	}
 	
 }
